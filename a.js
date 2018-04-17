7e1a354ca4edf6188e18e7e2fcba38f0b2884f86
wanghang <mxw_sunshine@163.com>
2018-04-17 20:19:20 +0800

    无缝轮播

---
+++ a.js
@@ -0,0 +1,69 @@
+let n
+初始化()
+setInterval(()=>{                
+    makeLeave(getImages(n))           //从视口离开
+    .one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget)) //跑到最后面
+    })
+    makeCurrent(getImages(n + 1))     //跑进视口
+    n+=1
+},1500)
+
+
+// 下面不用看哦
+function x(n) {
+    if (n > 4) {
+        n = n % 4
+        if (n === 0) {
+            n = 4
+        }
+    }
+    return n
+}
+function 初始化() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')                      //给除了他的兄弟添加enter
+}
+function getImages(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
+}
+function makeCurrent($node) {
+    return $node.removeClass('leave enter').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave')
+}
+
+
+// 初始化前
+// $('.images > img:nth-child(1)').addClass('current')
+// $('.images > img:nth-child(2)').addClass('enter')
+// $('.images > img:nth-child(3)').addClass('enter')
+// $('.images > img:nth-child(4)').addClass('enter')
+
+// let n = 1
+
+// setInterval(() => {
+//     $(`.images > img:nth-child(${x(n)}`).removeClass('current').addClass('leave')
+//         .one('transitionend', (e) => {
+//             $(e.currentTarget).removeClass('leave').addClass('enter')
+//         })
+//     $(`.images>img:nth-child(${x(n + 1)}`).removeClass('enter').addClass('current')
+//     n += 1
+// }, 1500)
+
+
+// function x(n) {
+//     if (n > 4) {
+//         n = n % 4
+//         if (n === 0) {
+//             n = 4
+//         }
+//     }
+//     return n
+// }
